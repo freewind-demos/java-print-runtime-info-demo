0e71cda72d785c014ffd7835c38cc14ea52f8f60
freewind <nowindlee@gmail.com>
2018-07-25 16:41:25 +0800

    runtime info

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-Java Hello World Demo
-=====================
+Java Show Runtime Info Demo
+===========================
+
+Get runtime and show info about:
+
+- cores
+- totalMemory
+- freeMemory
+- maxMemory
 
 Run `Hello.java` in your IDE.
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,9 +1,43 @@
 package demo;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.math.BigDecimal;
+
+import static demo.NumberUtils.convertToM;
+import static java.lang.Runtime.getRuntime;
+
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(new SystemInfo());
     }
 
 }
+
+class SystemInfo {
+
+    double cores = getRuntime().availableProcessors();
+    double totalMemory = convertToM(getRuntime().totalMemory());
+    double freeMemory = convertToM(getRuntime().freeMemory());
+    double maxMemory = convertToM(getRuntime().maxMemory());
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+}
+
+class NumberUtils {
+
+    static double convertToM(long num) {
+        return format(num * 1.0 / (1024 * 1024), 3);
+    }
+
+    static double format(double num, int decimal) {
+        return new BigDecimal(num).setScale(decimal, BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+
+}
+
+
